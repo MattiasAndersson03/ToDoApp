fb5e50dd3af6ebb28f73cd8250df78d4a1670659
Mattias Andersson <mattias0306@outlook.com>
2023-11-09 22:11:19 +0100

    uppdated

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 # http://localhost:5000/
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect
 import json
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+@app.errorhandler(404) # Fångar upp man skriver en endpoint som inte finns
+def not_found(error):
+    return jsonify({"message": "Ej giltig endpoint"}), 404
 
-def read_tasks(): # Används för att läsa uppgifterna från json filen
-    try:
-        with open('tasks.json', 'r') as file:
-            tasks = json.load(file)
-    except FileNotFoundError:
-        tasks = []
-    return tasks
+@app.route('/tasks/html', methods=['GET']) # Plockar fram templaten
+def tasks_html():
+    tasks = read_tasks()
+    return render_template('tasks.html', tasks=tasks)
 
-def save_tasks(tasks): # Sparar task till json filen
-    with open('tasks.json', 'w') as file:
-        json.dump(tasks, file, indent=4)
-
-
-@app.route('/tasks', methods=['GET', 'POST']) # plockar fram och kan även lägga till nya tasks
-def tasks():
+@app.route('/', methods=['GET', 'POST']) # Fångar upp task som skapa på sidan och skicka till save task
+def tasks_page():
     if request.method == 'POST':
         data = {
             'title': request.form['title'],
@@ -40,18 +32,57 @@ def tasks():
         return jsonify(new_task)
 
     tasks = read_tasks()
+    return render_template('tasks.html', tasks=tasks)
+
+def read_tasks(): # Används för att läsa uppgifterna från json filen
+    try:
+
+        with open('tasks.json', 'r') as file:
+            tasks = json.load(file)
+    except FileNotFoundError:
+        tasks = []
+    return tasks
+def save_tasks(tasks): # Sparar task till json filen
+    with open('tasks.json', 'w') as file:
+        json.dump(tasks, file, indent=4)
+
+@app.route('/tasks', methods=['GET', 'POST']) # plockar fram även lägar till nya task
+def tasks():
+    if request.method == 'POST':
+        data = {
+            'title': request.form['title'],
+            'category': request.form['category']
+        }
+        tasks = read_tasks()
+        new_task = {
+            'id': len(tasks) + 1,
+            'title': data['title'],
+            'completed': False,
+            'category': data['category']
+        }
+        tasks.append(new_task)
+        save_tasks(tasks)
+        return redirect('/tasks/html')
+
+    tasks = read_tasks()
     return jsonify(tasks)
 
-@app.route('/tasks/completed', methods=['GET']) # ploackar fråm de completed tasks
+@app.route('/tasks/completed', methods=['GET']) # ploackar fram completed tasks
 def completed_tasks():
     tasks = read_tasks()
     completed_tasks = [task for task in tasks if task['completed'] is True]
+
+    if not completed_tasks:
+        return "No completed tasks found"
     return jsonify(completed_tasks)
 
 @app.route('/tasks/incomplete', methods=['GET']) # plockar from de incompleted tasks
 def incomplete_tasks():
     tasks = read_tasks()
     incomplete_tasks = [task for task in tasks if task['completed'] is False]
+
+    if not incomplete_tasks:
+        return "No incompleted tasks found", 404
     return jsonify(incomplete_tasks)
 
 @app.route('/tasks/<int:task_id>', methods=['PUT']) # Ändrar en task med specifikt id
@@ -86,7 +117,15 @@ def delete_task(task_id):
         if task['id'] == task_id:
             tasks.remove(task)
             save_tasks(tasks)
-            return "Task deleted", 204
+            return jsonify({"message": "Task deleted"})
+    return "Task not found", 404
+
+@app.route('/tasks/<int:task_id>', methods=['GET']) #Hämtar en task med ett specifikt id
+def get_task(task_id):
+    tasks = read_tasks()
+    for task in tasks:
+        if task['id'] == task_id:
+            return jsonify(task)
     return "Task not found", 404
 
 @app.route('/tasks/categories', methods=['GET']) # Hämtar alla olika kategorier
@@ -99,24 +138,11 @@ def get_unique_categories():
 def get_tasks_by_category(category_name):
     tasks = read_tasks()
     tasks_in_category = [task for task in tasks if task['category'] == category_name]
-    return jsonify(tasks_in_category)
 
-@app.route('/tasks/<int:task_id>', methods=['GET']) #Hämtar en task med ett specifikt id
-def get_task(task_id):
-    tasks = read_tasks()
-    for task in tasks:
-        if task['id'] == task_id:
-            return jsonify(task)
-    return "Task not found", 404
+    if not tasks_in_category:
+        return "Category not found", 404
+    return jsonify(tasks_in_category)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-# GET /tasks ✅
-# POST /tasks ✅
-# GET /tasks/{task_id} ✅
-# DELETE /tasks/{task_id} ✅
-# PUT /tasks/{task_id} ✅
-# PUT /tasks/{task_id}/complete ✅
-# GET /tasks/categories/ ✅
-# GET /tasks/categories/{category_name} ✅
